efc507da8c91643c7f27bf2ac50f39b245c3f6df
Kareem <kareemsalah.fci@gmail.com>
2018-10-28 23:48:15 +0200

    Adds a unit test for selecting a choice in the dropdown

--- __tests__/components/Dropdown/dropdown.test.js
+++ __tests__/components/Dropdown/dropdown.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import configureStore from 'redux-mock-store';
 import Dropdown from '../../../src/components/Dropdown/dropdown.container';
@@ -36,8 +36,33 @@ describe('<Dropdown />', () => {
     const onClick = (params) => {};
     const wrapper = shallow(<Dropdown menuItems = {menuItems} onClick = {onClick}/>)
     const component = wrapper.dive();
+
     expect(toJson(component)).toMatchSnapshot();
     expect(wrapper.props().menuItems).toEqual(menuItems);
     expect(wrapper.props().onClick).toEqual(onClick);
+  });
+
+  test('invokes callback with selected item', () => {
+    const onClickMock = jest.fn();
+    const menuItems = [
+      {
+        text: 'Selection A',
+        value: 'selectA'
+      },
+      {
+        text: 'Selection B',
+        value: 'selectB'
+      },
+      {
+        text: 'Selection C',
+        value: 'selectC'
+      }
+    ];
+    const component = mount(<Dropdown menuItems = {menuItems} onClick = {onClickMock}/>);
+
+    component.find('.dropdown-item-btn[itemindex=1]').simulate('click');
+    component.unmount();
+
+    expect(onClickMock).toHaveBeenCalledWith(1);
   });
 });
